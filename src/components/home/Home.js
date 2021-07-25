947c3591ffbda0a619fafed692751c3b2f8a3e13
nguyendinhinit <48084269+nguyendinhinit@users.noreply.github.com>
2021-07-25 12:54:52 +0700

    delete
    delete

--- src/components/home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return (
-        <p>Display some kind of shiet in here</p>
-    );
-};
-
-export default Home;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import App from './App';
-import {ThemeProvider} from '@material-ui/core/styles';
-import theme from "./theme";
-
-ReactDOM.render(
-        <App/>,
-        document.getElementById("header")
-)
